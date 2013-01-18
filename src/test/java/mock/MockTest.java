ac58bfe59f3b5d393ac916bf6b47182b13d632eb
Stuart Holland <soulc3@gmail.com>
2013-01-18 19:01:38 +0000

    Added test flag to mock test.

--- src/test/java/mock/MockTest.java
+++ src/test/java/mock/MockTest.java
@@ -4,12 +4,14 @@ import junit.framework.TestCase;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.junit.Test;
 
 import agents.NomicAgent;
 
 public class MockTest extends TestCase {
 	Mockery context = new Mockery();
 	
+	@Test
 	public void mockTestInitialTest() {
 		final NomicAgent agent = context.mock(NomicAgent.class);
 		
