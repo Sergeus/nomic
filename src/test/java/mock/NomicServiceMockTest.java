3b80ce6d50d7d838b86a567179a07cba0fdcc039
Stuart Holland <soulc3@gmail.com>
2013-02-18 00:53:24 +0000

    Added new service test for moved functionality.

--- src/test/java/mock/NomicServiceMockTest.java
+++ src/test/java/mock/NomicServiceMockTest.java
@@ -12,32 +12,39 @@ import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import services.NomicService;
 import uk.ac.imperial.presage2.core.environment.EnvironmentSharedStateAccess;
 import uk.ac.imperial.presage2.core.event.EventBus;
+import actions.ProposeRuleAddition;
+import actions.ProposeRuleModification;
+import actions.ProposeRuleRemoval;
+import agents.NomicAgent;
 
 @SuppressWarnings("deprecation")
 @RunWith(JMock.class)
 public class NomicServiceMockTest extends TestCase {
 	Mockery context = new JUnit4Mockery();
 	
+	final EnvironmentSharedStateAccess ss = context.mock(EnvironmentSharedStateAccess.class);
+	final StatefulKnowledgeSession session = context.mock(StatefulKnowledgeSession.class);
+	final EventBus e = context.mock(EventBus.class);
+	final KnowledgeBase base = context.mock(KnowledgeBase.class);
+	
+	final String correctRule = "import agents.NomicAgent "
+			+ "rule \"Dynamic rule!\""
+			+ "when"
+			+ "	$agent : NomicAgent(SequentialID == 1)"
+			+ "then"
+			+ "	System.out.println(\"Found agent 1!\");"
+			+ "end";
+	
 	@Test
 	public void SingleStringRuleAdditionTest() {
-		String newRule = "import agents.NomicAgent "
-				+ "rule \"Dynamic rule!\""
-				+ "when"
-				+ "	$agent : NomicAgent(SequentialID == 1)"
-				+ "then"
-				+ "	System.out.println(\"Found agent 1!\");"
-				+ "end";
-		
-		final EnvironmentSharedStateAccess ss = context.mock(EnvironmentSharedStateAccess.class);
-		final StatefulKnowledgeSession session = context.mock(StatefulKnowledgeSession.class);
-		final EventBus e = context.mock(EventBus.class);
-		final KnowledgeBase base = context.mock(KnowledgeBase.class);
+		String newRule = correctRule;
 		
 		context.checking(new Expectations() {{
 			oneOf(e).subscribe(with(any(NomicService.class)));
@@ -58,10 +65,6 @@ public class NomicServiceMockTest extends TestCase {
 	
 	@Test
 	public void MultipleStringRuleAdditionTest() {
-		final EnvironmentSharedStateAccess ss = context.mock(EnvironmentSharedStateAccess.class);
-		final StatefulKnowledgeSession session = context.mock(StatefulKnowledgeSession.class);
-		final EventBus e = context.mock(EventBus.class);
-		final KnowledgeBase base = context.mock(KnowledgeBase.class);
 		
 		context.checking(new Expectations() {{
 			exactly(1).of(e).subscribe(with(any(NomicService.class)));
@@ -109,10 +112,6 @@ public class NomicServiceMockTest extends TestCase {
 	
 	@Test
 	public void RemoveRuleTest() {
-		final EnvironmentSharedStateAccess ss = context.mock(EnvironmentSharedStateAccess.class);
-		final StatefulKnowledgeSession session = context.mock(StatefulKnowledgeSession.class);
-		final EventBus e = context.mock(EventBus.class);
-		final KnowledgeBase base = context.mock(KnowledgeBase.class);
 		
 		final String packageName = "testPackage";
 		final String ruleName = "testRule";
@@ -126,6 +125,55 @@ public class NomicServiceMockTest extends TestCase {
 		
 		NomicService service = new NomicService(ss, session, e);
 		service.RemoveRule(packageName, ruleName);
+		
+		context.assertIsSatisfied();
+	}
+	
+	@Test
+	public void ApplyProposedRuleTest() {
+		context.setImposteriser(ClassImposteriser.INSTANCE);
+		
+		final NomicAgent mockAgent = context.mock(NomicAgent.class);
+		
+		final String newRule = correctRule;
+		
+		ProposeRuleAddition addition = new ProposeRuleAddition(mockAgent, newRule);
+		
+		context.checking(new Expectations() {{
+			oneOf(e).subscribe(with(any(NomicService.class)));
+			oneOf(session).getKnowledgeBase(); will(returnValue(base));
+			oneOf(base).addKnowledgePackages(with(any(Collection.class)));
+		}});
+		
+		NomicService service = new NomicService(ss, session, e);
+		service.ApplyRuleChange(addition);
+		
+		context.assertIsSatisfied();
+		
+		final String oldRuleName = "Old Rule Name";
+		final String oldRulePackage = "Old Rule Package";
+		ProposeRuleRemoval removal = new ProposeRuleRemoval(mockAgent, oldRuleName, oldRulePackage);
+		
+		context.checking(new Expectations() {{
+			oneOf(session).getKnowledgeBase(); will(returnValue(base));
+			oneOf(base).removeRule(oldRulePackage, oldRuleName);
+			oneOf(session).fireAllRules();
+		}});
+		
+		service.ApplyRuleChange(removal);
+		
+		context.assertIsSatisfied();
+		
+		ProposeRuleModification modification = new ProposeRuleModification(mockAgent, newRule, oldRuleName, oldRulePackage);
+		
+		context.checking(new Expectations() {{
+			exactly(2).of(session).getKnowledgeBase(); will(returnValue(base));
+			oneOf(base).removeRule(oldRulePackage, oldRuleName);
+			oneOf(base).addKnowledgePackages(with(any(Collection.class)));
+			oneOf(session).fireAllRules();
+		}});
+		
+		service.ApplyRuleChange(modification);
 		
 		context.assertIsSatisfied();
 	}
