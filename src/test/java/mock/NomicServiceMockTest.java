b3f2ab570c551f2720b1000e30b451c1221376aa
Stuart Holland <soulc3@gmail.com>
2013-06-04 15:40:56 +0100

    More test fixing.

--- src/test/java/mock/NomicServiceMockTest.java
+++ src/test/java/mock/NomicServiceMockTest.java
@@ -171,7 +171,6 @@ public class NomicServiceMockTest extends TestCase {
 		context.checking(new Expectations() {{
 			oneOf(session).getKnowledgeBase(); will(returnValue(base));
 			oneOf(base).removeRule(oldRulePackage, oldRuleName);
-			oneOf(serviceProvider).getEnvironmentService(with(RuleClassificationService.class));
 			will(returnValue(mockRuleClass));
 			oneOf(mockRuleClass).setActive(oldRuleName, false);
 		}});
@@ -186,7 +185,6 @@ public class NomicServiceMockTest extends TestCase {
 			exactly(2).of(session).getKnowledgeBase(); will(returnValue(base));
 			oneOf(base).removeRule(oldRulePackage, oldRuleName);
 			oneOf(base).addKnowledgePackages(with(any(Collection.class)));
-			oneOf(serviceProvider).getEnvironmentService(RuleClassificationService.class);
 			will(returnValue(mockRuleClass));
 			oneOf(mockRuleClass).setActive(oldRuleName, false);
 			oneOf(mockRuleClass).setActive(newRuleName, true);
